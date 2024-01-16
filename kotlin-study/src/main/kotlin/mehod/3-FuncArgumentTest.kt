80311abcb7e20c7f81c20014299aa67cb671d9e9
parkyountaek <yountaek11@gmail.com>
2024-01-16 23:37:36 +0900

    feat: FuncArgumentTest

--- kotlin-study/src/main/kotlin/mehod/3-FuncArgumentTest.kt
+++ kotlin-study/src/main/kotlin/mehod/3-FuncArgumentTest.kt
@@ -1,5 +1,18 @@
 package mehod
 
 fun main() {
+    println(sum(1, 3))
+    println(sum2(1, 2))
+    println(sum3(1, 3))
 
+    println(mul(sum(1, 3), 3))
+    println(mul(sum2(1, 3), 3))
+    println(mul(sum3(1, 3), 3))
 }
+
+fun sum(a: Int, b: Int) = a + b
+fun sum2(a: Int, b: Int): Int {
+    return a + b
+}
+val sum3 = {a: Int, b:Int -> a + b}
+fun mul(a: Int, b: Int) = a * b
